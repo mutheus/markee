fe1342f35a80236f065946f525b80a092d6b8907
Matheus Almeida <matheusalmeida016@gmail.com>
2021-09-15 10:30:20 -0400

    Add theme provider

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components'
 import { App } from 'app'
+import { theme } from 'resources/theme'
 
 export function Root () {
   return (
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   )
 }
