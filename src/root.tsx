74c45af4b2a5a1036b38e89487dde0d6ccac0e95
Matheus Almeida <matheusalmeida016@gmail.com>
2021-09-15 11:29:22 -0400

    Add global style

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,28 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { App } from 'app'
 import { theme } from 'resources/theme'
 
 export function Root () {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'DM Sans', sans-serif;
+    margin: 0;
+  }
+`
