c3b7a544e215e1a8dc481c74c732e3f3173960c9
Matheus Almeida <matheusalmeida016@gmail.com>
2021-09-18 19:38:01 -0400

    Add markdown parsing functionality

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,18 +1,29 @@
+import { useState, ChangeEvent } from 'react'
+import marked from 'marked'
 import * as S from './styles/content-style'
 
 export function Content () {
+  const [output, setOutput] = useState('')
+
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setOutput(e.target.value)
+  }
+
+  const createOutput = () => {
+    return { __html: marked(output) }
+  }
+
   return (
     <S.ContentWrapper>
       <S.Header>
         <S.FileIconPrimary />
+
         <S.InputText defaultValue='Contribut' />
       </S.Header>
       <S.Container>
-        <S.TextArea placeholder='Your markdown goes here...' />
-        <S.Output>
-          <h2>Bootcamp Brainn Co.</h2>
-          <p>Lorem ipsum dolor sit amet simet</p>
-        </S.Output>
+        <S.TextArea value={output} onChange={handleTextChange} placeholder='Your markdown goes here...' />
+
+        <S.Output dangerouslySetInnerHTML={createOutput()} />
       </S.Container>
     </S.ContentWrapper>
   )
